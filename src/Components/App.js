d5ee65362e497bf41d6e1a9452a528cf3214a8ff
Brian <briansterling93@yahoo.com>
2020-03-02 01:33:15 -0700

    styling

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,190 +15,185 @@ class App extends React.Component {
       sLength: 25,
       session: "Session",
       minutes: 25,
-      seconds: '00'
+      seconds: "00"
     };
   }
 
-
-incrementSession() {
-  this.setState(prevState => {
-   return { sLength: prevState.sLength + 1 }
-  })
-
-  this.setState(prevState => {
-    return { minutes: prevState.sLength }
-  })
-
-  this.setState({
-    seconds: `00`
-  })
-}
-
-decrementSession() {
-  this.setState(prevState => {
-    return { sLength: prevState.sLength - 1 }
-  })
-
-  this.setState(prevState => {
-    return { minutes: prevState.sLength }
-  })
-
-  if(this.state.sLength === 1) {
+  incrementSession() {
     this.setState(prevState => {
-      return { sLength: prevState.sLength + 1 }
-    })
+      return { sLength: prevState.sLength + 1 };
+    });
 
     this.setState(prevState => {
-      return { minutes: prevState.sLength}
-    })
-  }
-
-  this.setState({
-    seconds: `00`
-  })
-}
-
-startClock() {
+      return { minutes: prevState.sLength };
+    });
 
     this.setState({
-      minutes: this.state.sLength,
-      session: 'Session',
-      seconds: 59
-    })
+      seconds: `00`
+    });
+  }
+
+  decrementSession() {
+    this.setState(prevState => {
+      return { sLength: prevState.sLength - 1 };
+    });
 
     this.setState(prevState => {
-      return { minutes: prevState.minutes - 1 }
-    })
+      return { minutes: prevState.sLength };
+    });
+
+    if (this.state.sLength === 1) {
+      this.setState(prevState => {
+        return { sLength: prevState.sLength + 1 };
+      });
+
+      this.setState(prevState => {
+        return { minutes: prevState.sLength };
+      });
+    }
+
+    this.setState({
+      seconds: `00`
+    });
+  }
+
+  startClock() {
+    this.setState({
+      minutes: this.state.sLength,
+      session: "Session",
+      seconds: 59
+    });
+
+    this.setState(prevState => {
+      return { minutes: prevState.minutes - 1 };
+    });
 
     this.timer = setInterval(() => {
-    this.setState(prevState => {
-      return { seconds: prevState.seconds - 1 }
-    }) 
-
-    if(this.state.seconds === -1){
-      this.setState({
-        seconds: 59
-      })
-    
       this.setState(prevState => {
-        return { minutes: prevState.minutes - 1 }
-      })
-    } else {
-      if(this.state.minutes === 0 && this.state.seconds === 0) {
+        return { seconds: prevState.seconds - 1 };
+      });
+
+      if (this.state.seconds === -1) {
         this.setState({
-          minutes: '0',
-          seconds: '00',
-          session: 'Session Complete!'
-        })
-        clearInterval(this.timer)
-      }
-    }
-  }, 1000)
+          seconds: 59
+        });
 
-}
-
-resetClock() {
-  clearInterval(this.timer);
-  clearInterval(this.breakTimer);
-  this.setState({
-    minutes: 25,
-    seconds: `00`,
-    sLength: 25,
-    session: 'Session'
-  })
-}
-
-breakClock() {
-  this.setState({
-    session: 'Break',
-    minutes: this.state.bLength,
-    seconds: 59
-  })
-
-  this.setState(prevState => {
-    return { minutes: prevState.minutes - 1 }
-  })
-
-  this.breakTimer = setInterval(() => {
-    this.setState(prevState => {
-      return { seconds: prevState.seconds - 1}
-    })
-
-    if(this.state.seconds === -1) {
-      this.setState(prevState => {
-        return { 
-          seconds: 59,
-          minutes: prevState.minutes - 1
+        this.setState(prevState => {
+          return { minutes: prevState.minutes - 1 };
+        });
+      } else {
+        if (this.state.minutes === 0 && this.state.seconds === 0) {
+          this.setState({
+            minutes: "0",
+            seconds: "00",
+            session: "Session Complete!"
+          });
+          clearInterval(this.timer);
         }
-      })
-    } 
+      }
+    }, 1000);
+  }
 
-      if(this.state.seconds === 0 && this.state.minutes === 0) {
+  resetClock() {
+    clearInterval(this.timer);
+    clearInterval(this.breakTimer);
+    this.setState({
+      minutes: 25,
+      seconds: `00`,
+      sLength: 25,
+      session: "Session"
+    });
+  }
+
+  breakClock() {
+    this.setState({
+      session: "Break",
+      minutes: this.state.bLength,
+      seconds: 59
+    });
+
+    this.setState(prevState => {
+      return { minutes: prevState.minutes - 1 };
+    });
+
+    this.breakTimer = setInterval(() => {
+      this.setState(prevState => {
+        return { seconds: prevState.seconds - 1 };
+      });
+
+      if (this.state.seconds === -1) {
+        this.setState(prevState => {
+          return {
+            seconds: 59,
+            minutes: prevState.minutes - 1
+          };
+        });
+      }
+
+      if (this.state.seconds === 0 && this.state.minutes === 0) {
         this.setState({
           minutes: `0`,
           seconds: `00`,
-          session: 'Break Complete!'
-        })
+          session: "Break Complete!"
+        });
         clearInterval(this.breakTimer);
       }
-  }, 1000)
-}
+    }, 1000);
+  }
 
-incrementBreak() {
+  incrementBreak() {
+    this.setState({
+      session: "Break"
+    });
 
- this.setState({
-   session: 'Break'
- })
-  
- this.setState(prevState => {
-   return { bLength: prevState.bLength + 1 }
- })
+    this.setState(prevState => {
+      return { bLength: prevState.bLength + 1 };
+    });
 
- this.setState(prevState=> {
-   return { minutes: prevState.bLength }
- })
- 
-}
+    this.setState(prevState => {
+      return { minutes: prevState.bLength };
+    });
+  }
 
-decrementBreak() {
+  decrementBreak() {
+    this.setState({
+      session: "Break"
+    });
 
-  this.setState({
-    session: 'Break'
-  }) 
+    this.setState(prevState => {
+      return { bLength: prevState.bLength - 1 };
+    });
 
-  this.setState(prevState => {
-    return { bLength: prevState.bLength - 1 }
-  })
+    this.setState(prevState => {
+      return { minutes: prevState.bLength };
+    });
 
-  this.setState(prevState=> {
-   return { minutes: prevState.bLength }
- })
+    if (this.state.bLength === 1) {
+      this.setState({
+        bLength: 1
+      });
 
- if(this.state.bLength === 1) {
-   this.setState({
-     bLength: 1
-   })
-
-   this.setState(prevState=> {
-    return { minutes: prevState.bLength }
-  })
-   
- }
-}
+      this.setState(prevState => {
+        return { minutes: prevState.bLength };
+      });
+    }
+  }
 
   render() {
     return (
-      
       <div>
         <div id="title">
           <h1>Pomodoro Clock</h1>
         </div>
         <div id="lengthDiv">
           <div id="breakLength">
-            <h3>Break Length</h3>
+            <h3>
+              Break <span id="mq-title">Length</span>
+            </h3>
+
             <div id="breakBtnsDiv">
               <input
-                className='btn'
+                className="btn"
                 type="button"
                 id="increment"
                 value="+"
@@ -206,7 +201,7 @@ decrementBreak() {
               />
               <span id="span1">{this.state.bLength}</span>
               <input
-                className='btn'
+                className="btn"
                 type="button"
                 id="decrement"
                 value="-"
@@ -215,10 +210,12 @@ decrementBreak() {
             </div>
           </div>
           <div id="sessionLength">
-            <h3>Session Length</h3>
+            <h3>
+              Session <span id="mq-title">Length</span>
+            </h3>
             <div id="lengthBtnsDiv">
               <input
-              className='btn'
+                className="btn"
                 type="button"
                 id="increment"
                 value="+"
@@ -226,7 +223,7 @@ decrementBreak() {
               />
               <span id="span2">{this.state.sLength}</span>
               <input
-              className='btn'
+                className="btn"
                 type="button"
                 id="decrement"
                 value="-"
@@ -238,11 +235,23 @@ decrementBreak() {
         <div id="clockDiv">
           <div id="clock">
             <h1 id="clockTitle">{this.state.session}</h1>
-            <p id="clockTime">{this.state.minutes} : {this.state.seconds} </p>
+            <p id="clockTime">
+              {this.state.minutes} : {this.state.seconds}{" "}
+            </p>
             <div id="clockBtns">
-              <button id="startBtn" className='btn' onClick={this.startClock}>Start Session</button>
-              <button id="resetBtn" className='btn' onClick={this.resetClock}>Reset</button>
-              <button id="startBrkBtn" className='btn' onClick={this.breakClock}>Start Break</button>
+              <button id="startBtn" className="btn" onClick={this.startClock}>
+                Start Session
+              </button>
+              <button id="resetBtn" className="btn" onClick={this.resetClock}>
+                Reset
+              </button>
+              <button
+                id="startBrkBtn"
+                className="btn"
+                onClick={this.breakClock}
+              >
+                Start Break
+              </button>
             </div>
           </div>
         </div>
